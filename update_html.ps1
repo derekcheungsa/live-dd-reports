0fd2d7301a38fdbc98676e1b718c732cfad477f1
derekcheungsa <122410431+derekcheungsa@users.noreply.github.com>
2023-01-26 17:14:24 -0500

    daily update

--- update_html.ps1
+++ update_html.ps1
@@ -1,6 +1,6 @@
 # List of parameters
 #$param_list = "AR AROC CMRE EGY EPR EPD ET FLNG FTCO HDSN GSL KNTK IBM INSW MP MPW MPLX TRTN OXY V VET ZIM"
-$param_list = "V"
+$param_list = "IBM"
 
 
 # Splitting the string
