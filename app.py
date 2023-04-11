c663a48137166f8489409806d0c5d865d8721b46
derekcheungsa <122410431+derekcheungsa@users.noreply.github.com>
2023-04-10 23:26:26 -0400

    Added Q&A chat

--- app.py
+++ app.py
@@ -13,6 +13,9 @@ from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 import certifi
+import numpy as np
+import openai
+from openai.embeddings_utils import distances_from_embeddings, cosine_similarity
 
 #Use this dict to add term to the query, you can use OR in the string for more than one term
 #
@@ -32,6 +35,72 @@ access_token_secret  = os.environ['TWITTER_ACCESS_SECRET']
 fmp_key = os.environ['FMP_KEY']
 
 templates = Jinja2Templates(directory="public/templates")
+df=pd.read_csv('public/assets/openai/embeddings.csv', index_col=0)
+df['embeddings'] = df['embeddings'].apply(eval).apply(np.array)
+
+
+
+def create_context(
+    question, df, max_len=1800, size="ada"
+):
+    """
+    Create a context for a question by finding the most similar context from the dataframe
+    """
+
+    # Get the embeddings for the question
+    q_embeddings = openai.Embedding.create(input=question, engine='text-embedding-ada-002')['data'][0]['embedding']
+
+    # Get the distances from the embeddings
+    df['distances'] = distances_from_embeddings(q_embeddings, df['embeddings'].values, distance_metric='cosine')
+
+
+    returns = []
+    cur_len = 0
+
+    # Sort by distance and add the text to the context until the context is too long
+    for i, row in df.sort_values('distances', ascending=True).iterrows():
+        
+        # Add the length of the text to the current length
+        cur_len += row['n_tokens'] + 4
+        
+        # If the context is too long, break
+        if cur_len > max_len:
+            break
+        
+        # Else add it to the text that is being returned
+        returns.append(row["text"])
+
+    # Return the context
+    return "\n\n###\n\n".join(returns)
+
+
+@app.get("/get_openai_completion/{message}", response_class=HTMLResponse)
+async def get_openai_completion(message: str, request: Request):
+    context = create_context(
+        message,
+        df,
+        max_len=1800,
+        size="ada",
+    )
+    # If debug, print the raw model response
+
+    try:
+        # Create a completions using the questin and context
+        response = openai.Completion.create(
+            prompt=f"Answer the question based on the context below\n\nContext: {context}\n\n---\n\nQuestion: {message}\nAnswer:",
+            temperature=0,
+            max_tokens=1500,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0,
+            stop=None,
+            model="text-davinci-003",
+        )
+        return response["choices"][0]["text"].strip()
+    except Exception as e:
+        print(e)
+        return ""
+
 
 @app.get("/get_ratios_quarterly/{symbol_name}", response_class=HTMLResponse)
 async def get_ratios_quarterly(symbol_name: str, request: Request):
